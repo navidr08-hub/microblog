98dc5847f9e700a9de391c29486cd3245a76d597
Taskin Abdur-Rahman <navidrahman5@gmail.com>
2025-09-25 15:17:20 -0400

    v2.0 complete

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,18 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'username': 'Taskin'}
+    posts = [
+        {
+            'author': {'username': 'Safwan'},
+            'body': 'Had a great time at the BBQ!'
+        },
+        {
+            'author': {'username': 'Hisham'},
+            'body': 'Beautiful day in Jeddah!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
