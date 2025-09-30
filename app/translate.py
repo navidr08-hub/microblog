1e8e5a380416ef95c45b6b306d85bd9f4750af85
Taskin Abdur-Rahman <navidrahman5@gmail.com>
2025-09-29 20:22:45 -0400

    v14.0 complete, need to update README.md with some info

---
+++ app/translate.py
@@ -0,0 +1,25 @@
+import requests
+from flask_babel import _
+from app import app
+
+def translate(text, source_language, dest_language):
+    if 'MS_TRANSLATOR_KEY' not in app.config or not app.config['MS_TRANSLATOR_KEY']:
+        return _('Error: the translation service is not configured.')
+
+    headers = {
+        'Ocp-Apim-Subscription-Key': app.config['MS_TRANSLATOR_KEY'],
+        'Ocp-Apim-Subscription-Region': app.config['MS_TRANSLATOR_REGION'],  # add this in config
+        'Content-Type': 'application/json'
+    }
+
+    url = (
+        'https://api.cognitive.microsofttranslator.com/translate'
+        f'?api-version=3.0&from={source_language}&to={dest_language}'
+    )
+
+    r = requests.post(url, headers=headers, json=[{'Text': text}])
+
+    if r.status_code != 200:
+        return _('Error: the translation service failed.\nError code: ' + str(r.status_code))
+
+    return r.json()[0]['translations'][0]['text']
